727f3e3ea85ddd95b617c29d13c3daadecaa49e7
nimigeanconstantinion <constantin.nimigean@gmail.com>
2026-03-26 21:58:36 +0200

    enable scheduling

--- src/main/java/com/example/importer/ImporterApplication.java
+++ src/main/java/com/example/importer/ImporterApplication.java
@@ -4,10 +4,11 @@ import com.example.importer.config.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigurationProperties(CorsProperties.class)
-
+@EnableScheduling
 public class ImporterApplication {
 
 	public static void main(String[] args) {
